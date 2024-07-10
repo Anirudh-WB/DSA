d4319b4d4ad6c2e357e09a716874322fc18f891c
Anirudh Gupta <anirudh.gupta@wonderbiz.in>
2024-07-10 11:01:54 +0530

    first commit

---
+++ Krishnamurthy number/Krishnamurthy number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krishnamurthy_number
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to check");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int temp = n;
+            int km = 0;
+            while (temp > 0)
+            {
+                int rem = temp % 10;
+
+                km += Fact(rem);
+
+                temp /= 10;
+            }
+
+            Console.WriteLine(km == n ? "It is Krishnamurthy number" : "It is not a Krishnamurthy number");
+        }
+
+        public static int Fact(int n)
+        {
+            int fact = 1;
+            while (n > 0)
+            {
+                fact *= n;
+                n--;
+            }
+
+            return fact;
+        }
+    }
+}
---
+++ Valid Anagram/Valid Anagram/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string");
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter second string");
+            string str2 = Console.ReadLine();
+
+            str1 = new string(str1.OrderBy(c => c).ToArray());
+            str2 = new string(str2.OrderBy(c => c).ToArray());
+
+            bool fl = true;
+
+            if (str1.Length == str2.Length)
+            {
+                for (int i = 0; i < str2.Length; i++)
+                {
+                    if (str1[i] != str2[i])
+                    {
+                        fl = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                fl = false;
+            }
+
+            Console.WriteLine(fl ? "It is Valid Anagram" : "It is not a Vslid Anagram");
+        }
+    }
+}
